4e011f413e17a54e336838e2956f5d134f59937d
l <l@ls-MacBook-Pro.local>
2017-07-13 17:56:44 -0500

    first commit

---
+++ DelegateDemo/SelectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SelectionViewController.swift
+//  DelegateDemo
+//
+//  Created by l on 7/8/17.
+//  Copyright © 2017 wenova. All rights reserved.
+//
+
+import UIKit
+
+class SelectionViewController: NSObject {
+
+}
